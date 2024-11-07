0a166d7869547daa897288a603a497e803d96e00
23ssuss <117702620+23ssuss@users.noreply.github.com>
2024-11-07 23:28:44 +0300

    breaking-best-and-worst-records.rs

--- task20/src/breaking-best-and-worst-records.rs
+++ task20/src/breaking-best-and-worst-records.rs
@@ -2,15 +2,23 @@ use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, Write};
 
-/*
- * Complete the 'breakingRecords' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts INTEGER_ARRAY scores as parameter.
- */
-
 fn breakingRecords(scores: &[i32]) -> Vec<i32> {
+    let mut highest_score = scores[0];
+    let mut lowest_score = scores[0];
+    let mut highest_count = 0;
+    let mut lowest_count = 0;
 
+    for &score in scores.iter().skip(1) {
+        if score > highest_score {
+            highest_score = score;
+            highest_count += 1;
+        } else if score < lowest_score {
+            lowest_score = score;
+            lowest_count += 1;
+        }
+    }
+
+    vec![highest_count, lowest_count]
 }
 
 fn main() {
