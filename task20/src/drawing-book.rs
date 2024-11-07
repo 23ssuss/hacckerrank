428ec52358cf07205936e15a890f315ca9c9e93a
23ssuss <117702620+23ssuss@users.noreply.github.com>
2024-11-07 23:32:16 +0300

    drawing-book.rs

--- task20/src/drawing-book.rs
+++ task20/src/drawing-book.rs
@@ -2,17 +2,10 @@ use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, Write};
 
-/*
- * Complete the 'pageCount' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts following parameters:
- *  1. INTEGER n
- *  2. INTEGER p
- */
-
 fn pageCount(n: i32, p: i32) -> i32 {
-
+    let from_front = p / 2;
+    let from_back = (n / 2 - p / 2);
+    std::cmp::min(from_front, from_back)
 }
 
 fn main() {
