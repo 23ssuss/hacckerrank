768b5b07305b653ed033d9c02ae4f7e9b16e311b
23ssuss <117702620+23ssuss@users.noreply.github.com>
2024-11-07 23:31:12 +0300

    day-of-the-programmer.rs

--- task20/src/day-of-the-programmer.rs
+++ task20/src/day-of-the-programmer.rs
@@ -2,15 +2,16 @@ use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, Write};
 
-/*
- * Complete the 'dayOfProgrammer' function below.
- *
- * The function is expected to return a STRING.
- * The function accepts INTEGER year as parameter.
- */
-
 fn dayOfProgrammer(year: i32) -> String {
+    if year == 1918 {
+        return "26.09.1918".to_string();
+    }
 
+    if (year < 1918 && year % 4 == 0) || (year > 1918 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))) {
+        return format!("12.09.{}", year);
+    } else {
+        return format!("13.09.{}", year);
+    }
 }
 
 fn main() {
